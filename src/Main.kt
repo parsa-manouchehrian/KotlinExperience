1a768163ecf6d27744d253c6c185550801eea787
parsaManouchehrian <=>
2020-07-25 16:37:10 -0700

    test extention

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,15 @@
 
+fun Person.WhoAreYou(ID: Int) {
+    println("Name: ${this.firstname + " " + this.Lastname} Height: ${this.height} #ID: $ID")
+}
+
+var Person.Lastname : String
+    get() {
+        return "${this.firstname}ian"
+    }
+set(value) {
+}
+
     fun main(args: Array<String>) {
         println("tse");
 
@@ -8,9 +19,12 @@
             .SayBye()
 
         person.height = 2
+        person.firstname = "Parsa"
 
         println("${person.height}")
 
+        person.Lastname = ""
+        person.WhoAreYou(0)
 
         var d = D()
 
